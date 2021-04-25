d5d2ffec0dadb90acdbc05295b2ab31685fea35f
Paula Mühlbach <pollam@gmail.com>
2021-04-25 16:42:54 -0300

    adding serverErrorMessagesComponent to forms

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,13 +5,15 @@ import { RouterModule } from '@angular/router';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
 import { FormFieldErrorComponent } from './components/form-field-error/form-field-error.component';
+import { ServerErrorMessagesComponent } from './components/server-error-messages/server-error-messages.component';
 
 
 @NgModule({
   declarations: [
     BreadCrumbComponent,
     PageHeaderComponent,
-    FormFieldErrorComponent
+    FormFieldErrorComponent,
+    ServerErrorMessagesComponent
   ],
   imports: [
     CommonModule,
@@ -24,7 +26,8 @@ import { FormFieldErrorComponent } from './components/form-field-error/form-fiel
     RouterModule,
     BreadCrumbComponent,
     PageHeaderComponent,
-    FormFieldErrorComponent
+    FormFieldErrorComponent,
+    ServerErrorMessagesComponent
   ]
 })
 export class SharedModule { }
